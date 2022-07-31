5e2a347f1bfa9e82760cb12ae969ef935c810aff
Wjt1127 <1243254658@qq.com>
2022-07-31 11:56:52 +0800

    createobj and POT in file

--- createObj_test.c
+++ createObj_test.c
@@ -18,7 +18,7 @@
 #define ID_BYTES 8 //如果需要修改oid为128位，则改为16
 
 /* 通过以下函数获得uuid,并以此标识obj */
-unsigned long Oid_generate()
+uint64_t Oid_generate()
 {
      uuid_t uu; // uuid_t类型就是 char [16]
      uuid_generate_time(uu);
@@ -26,7 +26,7 @@ unsigned long Oid_generate()
      int i;
      if (ID_BYTES == 8) //返回64位ID
      {
-          unsigned long Oid = 0; //需要返回的Oid
+          uint64_t Oid = 0; //需要返回的Oid
           for (i = 0; i < 8; i++)
           {
                Oid = (Oid << 8) + uu[i];
@@ -37,7 +37,7 @@ unsigned long Oid_generate()
      {
           if (ID_BYTES == 16) //返回128位ID
           {
-               unsigned long Oid[2] = {0}; //需要返回的Oid
+               uint64_t Oid[2] = {0}; //需要返回的Oid
                for (i = 0; i < 8; i++)
                {
                     Oid[0] = (Oid[0] << 8) + uu[i];
@@ -64,20 +64,30 @@ PMEMoid CreateObj(int fd,off_t offset)
      PMEMoid obj;
      OTE add_ote;
 
-     if((fd=open("POT",O_RDWR)) < 0){  /* POT是 Persistent Object Table
+     /* 后期需要去除打开pot文件这段 实现一个函数获得POT的基址 */
+     FILE* fd_pot;
+     if((fd_pot=fopen("POT","a+")) == NULL){  /* POT是 Persistent Object Table
                为了简化前期的工作 使用POT文件来记录object 到 offset的映射*/
-          perror("open");
+          perror("fopen");
 		exit(1);
      }
 
+     /* 获得 fd 对应的inode numeber */
      struct files_struct *files = current->files;  //获取本进程的files结构体
      file = fcheck_files(files, fd); //根据files_struct结构获取file结构体
-
      struct inode *finode = file->f_inode;
      unsigned long ino = finode->i_ino;  //获得 inode number
      
-     obj.objid = Oid_generate()
+     /* 初始化对象句柄 */
+     obj.objid = Oid_generate();
+     obj.off = offset;
 
+     /* 在POT中添加这条映射 */
+     add_ote.obj = obj;
+     add_ote.i_ino = ino;
+     fwrite(add_ote,sizeof(add_ote),1,fd_pot);  //后续需修改
+
+     return obj;
 }
 
 
