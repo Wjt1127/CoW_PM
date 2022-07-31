d82c352ce6c12e65efcb90e72620f79496f42f19
Wjt1127 <1243254658@qq.com>
2022-07-31 14:04:52 +0800

    define createobj

--- obj.c
+++ obj.c
@@ -1,310 +1,75 @@
 /*
  * 先跳过PM的分配问题，在用户态建立一个文件来记录POT(persisten-obj-table)。
  */
+#include <obj.h>
 
+/* 通过以下函数获得uuid,并以此标识obj */
+uint64_t Oid_generate()
+{
+     uuid_t uu; // uuid_t类型就是 char [16]
+     uuid_generate_time(uu);
 
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
-#include <linux/kernel.h>
-#include <linux/slab.h>
-#include <linux/backing-dev.h>
-#include <linux/mm.h>
-#include <linux/vmacache.h>
-#include <linux/shm.h>
-#include <linux/mman.h>
-#include <linux/pagemap.h>
-#include <linux/swap.h>
-#include <linux/syscalls.h>
-#include <linux/capability.h>
-#include <linux/init.h>
-#include <linux/file.h>
-#include <linux/fs.h>
-#include <linux/personality.h>
-#include <linux/security.h>
-#include <linux/hugetlb.h>
-#include <linux/shmem_fs.h>
-#include <linux/profile.h>
-#include <linux/export.h>
-#include <linux/mount.h>
-#include <linux/mempolicy.h>
-#include <linux/rmap.h>
-#include <linux/mmu_notifier.h>
-#include <linux/mmdebug.h>
-#include <linux/perf_event.h>
-#include <linux/audit.h>
-#include <linux/khugepaged.h>
-#include <linux/uprobes.h>
-#include <linux/rbtree_augmented.h>
-#include <linux/notifier.h>
-#include <linux/memory.h>
-#include <linux/printk.h>
-#include <linux/userfaultfd_k.h>
-#include <linux/moduleparam.h>
-#include <linux/pkeys.h>
-#include <linux/oom.h>
-#include <linux/sched/mm.h>
-
-#include <linux/uaccess.h>
-#include <asm/cacheflush.h>
-#include <asm/tlb.h>
-#include <asm/mmu_context.h>
-
-#include "internal.h"
-
-#ifndef arch_mmap_check
-#define arch_mmap_check(addr, len, flags)	(0)
-#endif
-
-#ifdef CONFIG_HAVE_ARCH_MMAP_RND_BITS
-const int mmap_rnd_bits_min = CONFIG_ARCH_MMAP_RND_BITS_MIN;
-const int mmap_rnd_bits_max = CONFIG_ARCH_MMAP_RND_BITS_MAX;
-int mmap_rnd_bits __read_mostly = CONFIG_ARCH_MMAP_RND_BITS;
-#endif
-#ifdef CONFIG_HAVE_ARCH_MMAP_RND_COMPAT_BITS
-const int mmap_rnd_compat_bits_min = CONFIG_ARCH_MMAP_RND_COMPAT_BITS_MIN;
-const int mmap_rnd_compat_bits_max = CONFIG_ARCH_MMAP_RND_COMPAT_BITS_MAX;
-int mmap_rnd_compat_bits __read_mostly = CONFIG_ARCH_MMAP_RND_COMPAT_BITS;
-#endif
+     int i;
+     if (ID_BYTES == 8) //返回64位ID
+     {
+          uint64_t Oid = 0; //需要返回的Oid
+          for (i = 0; i < 8; i++)
+          {
+               Oid = (Oid << 8) + uu[i];
+          }
+          return Oid;
+     }
+     if (0) //如果需要128位将函数返回类型修改
+     {
+          if (ID_BYTES == 16) //返回128位ID
+          {
+               uint64_t Oid[2] = {0}; //需要返回的Oid
+               for (i = 0; i < 8; i++)
+               {
+                    Oid[0] = (Oid[0] << 8) + uu[i];
+               }
+               for (i = 8; i < 16; i++)
+               {
+                    Oid[1] = (Oid[1] << 8) + uu[i];
+               }
+               return Oid;
+          }
+     }
+}
 
 /*
- * The caller must hold down_write(&current->mm->mmap_sem).
+ * 根据输入的 fd和 offset返回对应的对象句柄
+ * 并在POT中建立 该对象到 offset的映射条目
  */
-unsigned long do_mmap(struct file *file, unsigned long addr,
-			unsigned long len, unsigned long prot,
-			unsigned long flags, vm_flags_t vm_flags,
-			unsigned long pgoff, unsigned long *populate,
-			struct list_head *uf)
+PMEMoid CreateObj(int fd,off_t offset)
 {
-	struct mm_struct *mm = current->mm;
-	int pkey = 0;
+     int fd;
+     struct file *file;
+     PMEMoid obj;
+     OTE add_ote;
 
-	*populate = 0;
+     /* 后期需要去除打开pot文件这段 实现一个函数获得POT的基址 */
+     FILE* fd_pot;
+     if((fd_pot=fopen("POT","a+")) == NULL){  /* POT是 Persistent Object Table
+               为了简化前期的工作 使用POT文件来记录object 到 offset的映射*/
+          perror("fopen");
+		exit(1);
+     }
 
-	if (!len)
-		return -EINVAL;
+     /* 获得 fd 对应的inode numeber */
+     struct files_struct *files = current->files;  //获取本进程的files结构体
+     file = fcheck_files(files, fd); //根据files_struct结构获取file结构体
+     struct inode *finode = file->f_inode;
+     unsigned long ino = finode->i_ino;  //获得 inode number
+     
+     /* 初始化对象句柄 */
+     obj.objid = Oid_generate();
+     obj.off = offset;
 
-	/*
-	 * Does the application expect PROT_READ to imply PROT_EXEC?
-	 *
-	 * (the exception is when the underlying filesystem is noexec
-	 *  mounted, in which case we dont add PROT_EXEC.)
-	 */
-	if ((prot & PROT_READ) && (current->personality & READ_IMPLIES_EXEC))
-		if (!(file && path_noexec(&file->f_path)))
-			prot |= PROT_EXEC;
+     /* 在POT中添加这条映射 */
+     add_ote.obj = obj;
+     add_ote.i_ino = ino;
+     fwrite(add_ote,sizeof(add_ote),1,fd_pot);  //后续需修改
 
-	/* force arch specific MAP_FIXED handling in get_unmapped_area */
-	if (flags & MAP_FIXED_NOREPLACE)
-		flags |= MAP_FIXED;
-
-	if (!(flags & MAP_FIXED))
-		addr = round_hint_to_min(addr);
-
-	/* Careful about overflows.. */
-	len = PAGE_ALIGN(len);
-	if (!len)
-		return -ENOMEM;
-
-	/* offset overflow? */
-	if ((pgoff + (len >> PAGE_SHIFT)) < pgoff)
-		return -EOVERFLOW;
-
-	/* Too many mappings? */
-	if (mm->map_count > sysctl_max_map_count)
-		return -ENOMEM;
-
-	/* Obtain the address to map to. we verify (or select) it and ensure
-	 * that it represents a valid section of the address space.
-	 */
-	addr = get_unmapped_area(file, addr, len, pgoff, flags);
-	if (offset_in_page(addr))
-		return addr;
-
-	if (flags & MAP_FIXED_NOREPLACE) {
-		struct vm_area_struct *vma = find_vma(mm, addr);
-
-		if (vma && vma->vm_start < addr + len)
-			return -EEXIST;
-	}
-
-	if (prot == PROT_EXEC) {
-		pkey = execute_only_pkey(mm);
-		if (pkey < 0)
-			pkey = 0;
-	}
-
-	/* Do simple checking here so the lower-level routines won't have
-	 * to. we assume access permissions have been handled by the open
-	 * of the memory object, so we don't do any here.
-	 */
-	vm_flags |= calc_vm_prot_bits(prot, pkey) | calc_vm_flag_bits(flags) |
-			mm->def_flags | VM_MAYREAD | VM_MAYWRITE | VM_MAYEXEC;
-
-	if (flags & MAP_LOCKED)
-		if (!can_do_mlock())
-			return -EPERM;
-
-	if (mlock_future_check(mm, vm_flags, len))
-		return -EAGAIN;
-
-	if (file) {
-		struct inode *inode = file_inode(file);
-		unsigned long flags_mask;
-
-		if (!file_mmap_ok(file, inode, pgoff, len))
-			return -EOVERFLOW;
-
-		flags_mask = LEGACY_MAP_MASK | file->f_op->mmap_supported_flags;
-
-		switch (flags & MAP_TYPE) {
-		case MAP_SHARED:
-			/*
-			 * Force use of MAP_SHARED_VALIDATE with non-legacy
-			 * flags. E.g. MAP_SYNC is dangerous to use with
-			 * MAP_SHARED as you don't know which consistency model
-			 * you will get. We silently ignore unsupported flags
-			 * with MAP_SHARED to preserve backward compatibility.
-			 */
-			flags &= LEGACY_MAP_MASK;
-			/* fall through */
-		case MAP_SHARED_VALIDATE:
-			if (flags & ~flags_mask)
-				return -EOPNOTSUPP;
-			if (prot & PROT_WRITE) {
-				if (!(file->f_mode & FMODE_WRITE))
-					return -EACCES;
-				if (IS_SWAPFILE(file->f_mapping->host))
-					return -ETXTBSY;
-			}
-
-			/*
-			 * Make sure we don't allow writing to an append-only
-			 * file..
-			 */
-			if (IS_APPEND(inode) && (file->f_mode & FMODE_WRITE))
-				return -EACCES;
-
-			/*
-			 * Make sure there are no mandatory locks on the file.
-			 */
-			if (locks_verify_locked(file))
-				return -EAGAIN;
-
-			vm_flags |= VM_SHARED | VM_MAYSHARE;
-			if (!(file->f_mode & FMODE_WRITE))
-				vm_flags &= ~(VM_MAYWRITE | VM_SHARED);
-
-			/* fall through */
-		case MAP_PRIVATE:
-			if (!(file->f_mode & FMODE_READ))
-				return -EACCES;
-			if (path_noexec(&file->f_path)) {
-				if (vm_flags & VM_EXEC)
-					return -EPERM;
-				vm_flags &= ~VM_MAYEXEC;
-			}
-
-			if (!file->f_op->mmap)
-				return -ENODEV;
-			if (vm_flags & (VM_GROWSDOWN|VM_GROWSUP))
-				return -EINVAL;
-			break;
-
-		default:
-			return -EINVAL;
-		}
-	} else {
-		switch (flags & MAP_TYPE) {
-		case MAP_SHARED:
-			if (vm_flags & (VM_GROWSDOWN|VM_GROWSUP))
-				return -EINVAL;
-			/*
-			 * Ignore pgoff.
-			 */
-			pgoff = 0;
-			vm_flags |= VM_SHARED | VM_MAYSHARE;
-			break;
-		case MAP_PRIVATE:
-			/*
-			 * Set pgoff according to addr for anon_vma.
-			 */
-			pgoff = addr >> PAGE_SHIFT;
-			break;
-		default:
-			return -EINVAL;
-		}
-	}
-
-	/*
-	 * Set 'VM_NORESERVE' if we should not account for the
-	 * memory use of this mapping.
-	 */
-	if (flags & MAP_NORESERVE) {
-		/* We honor MAP_NORESERVE if allowed to overcommit */
-		if (sysctl_overcommit_memory != OVERCOMMIT_NEVER)
-			vm_flags |= VM_NORESERVE;
-
-		/* hugetlb applies strict overcommit unless MAP_NORESERVE */
-		if (file && is_file_hugepages(file))
-			vm_flags |= VM_NORESERVE;
-	}
-
-	addr = mmap_region(file, addr, len, vm_flags, pgoff, uf);
-	if (!IS_ERR_VALUE(addr) &&
-	    ((vm_flags & VM_LOCKED) ||
-	     (flags & (MAP_POPULATE | MAP_NONBLOCK)) == MAP_POPULATE))
-		*populate = len;
-	return addr;
+     return obj;
 }
-
-unsigned long ksys_mmap_pgoff(unsigned long addr, unsigned long len,
-			      unsigned long prot, unsigned long flags,
-			      unsigned long fd, unsigned long pgoff)
-{
-	struct file *file = NULL;
-	unsigned long retval;
-
-	if (!(flags & MAP_ANONYMOUS)) {
-		audit_mmap_fd(fd, flags);
-		file = fget(fd);
-		if (!file)
-			return -EBADF;
-		if (is_file_hugepages(file))
-			len = ALIGN(len, huge_page_size(hstate_file(file)));
-		retval = -EINVAL;
-		if (unlikely(flags & MAP_HUGETLB && !is_file_hugepages(file)))
-			goto out_fput;
-	} else if (flags & MAP_HUGETLB) {
-		struct user_struct *user = NULL;
-		struct hstate *hs;
-
-		hs = hstate_sizelog((flags >> MAP_HUGE_SHIFT) & MAP_HUGE_MASK);
-		if (!hs)
-			return -EINVAL;
-
-		len = ALIGN(len, huge_page_size(hs));
-		/*
-		 * VM_NORESERVE is used because the reservations will be
-		 * taken when vm_ops->mmap() is called
-		 * A dummy user value is used because we are not locking
-		 * memory so no accounting is necessary
-		 */
-		file = hugetlb_file_setup(HUGETLB_ANON_FILE, len,
-				VM_NORESERVE,
-				&user, HUGETLB_ANONHUGE_INODE,
-				(flags >> MAP_HUGE_SHIFT) & MAP_HUGE_MASK);
-		if (IS_ERR(file))
-			return PTR_ERR(file);
-	}
-
-	flags &= ~(MAP_EXECUTABLE | MAP_DENYWRITE);
-
-	retval = vm_mmap_pgoff(file, addr, len, prot, flags, pgoff);
-out_fput:
-	if (file)
-		fput(file);
-	return retval;
-}
-
-
-
--- obj.h
+++ obj.h
@@ -2,8 +2,16 @@
  *   实现obj结构体的定义和相关函数的声明
  */
 
-#include <stddef.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <sys/time.h>
+#include <uuid/uuid.h>
+#include <linux/sched.h>
+
+#include <asm/current.h>
 
 /*
  * Object handle
@@ -22,5 +30,15 @@ typedef struct __OTE
      //使用 ino + offset
      PMEMoid obj; //其中包含了offset
      unsigned long i_ino;  //inode number
-}OTE;
+} OTE;
 
+
+#define ID_BYTES 8 //如果需要修改oid为128位，则改为16
+
+uint64_t Oid_generate(); /* 通过以下函数获得uuid,并以此标识obj */
+
+/*
+ * 根据输入的 fd和 offset返回对应的对象句柄
+ * 并在POT中建立 该对象到 offset的映射条目
+ */
+PMEMoid CreateObj(int fd,off_t offset);
